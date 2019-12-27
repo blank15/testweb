f81519d5dcaf0c79dbeaf61b4131a2dc74b55198
Yudistiro Septian Dwi Saputro <yudistiro6@gmail.com>
2019-12-27 18:55:10 +0700

    change config db

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "belajar.database.windows.net";
+    $user = "belajar";
+    $pass = "@Yudistiro15";
+    $db = "db_belajar";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
